78cab5bbad1e3eb61fe56e0a8328ce3d1f78ceb0
Lawal Lanre E <larrstonegroup@gmail.com>
2017-07-03 13:25:43 +0100

    complete monday - getPrimes challenge

--- monday_getPrimes/app/index.js
+++ monday_getPrimes/app/index.js
@@ -1,15 +1,25 @@
 'use strict';
 
 let getPrimes = (number) => {
+  let primes = [];
+
   if (number <= 0)
-    return "invalid number";
+    return primes;
 
-	let primes = [];
+	for (let i = 2; i <= number; i++) {
+    if (isPrime(i))
+      primes.push(i);
+  }
+	
+  return primes;
+}
 
-	for (let i = 0; i < number; i++) {
-		
-	}
-	return primes;
+const isPrime = (number) => {
+  for (let k = 2; k <= Math.sqrt(number); k++) {
+    if (number % k == 0) 
+      return false;
+  }
+  return true;
 }
 
 module.exports = getPrimes
--- monday_getPrimes/test/test.js
+++ monday_getPrimes/test/test.js
@@ -7,11 +7,15 @@ let expected = [];
 
 describe("getPrimes() for invalid inputs", function() {
     it("less than zero", function() {
-        expect(getPrimes(-1)).to.equal("invalid number");
+        expect(getPrimes(-1).toString()).to.equal(expected.toString());
     });
     
     it("equal to zero", function() {
-        expect(getPrimes(0)).to.equal("invalid number");
+        expect(getPrimes(0).toString()).to.equal(expected.toString());
+    });
+
+    it("equal to 1", function() {
+        expect(getPrimes(1).toString()).to.equal(expected.toString());
     });
  });
 
@@ -19,8 +23,61 @@ describe("getPrimes() for invalid inputs", function() {
 describe("getPrimes() for valid inputs", function() {
     it("5", function() {
 
-    expected = [];
+        expected = [2, 3, 5];
 
-    expect(getPrimes(5)).to.equal(expected);
+        expect(getPrimes(5).toString()).to.equal(expected.toString());
+    });
+
+    it("19", function() {
+
+        expected = [2, 3, 5, 7, 11, 13, 17, 19];
+
+        expect(getPrimes(19).toString()).to.equal(expected.toString());
+    });
+
+
+    it("50", function() {
+
+        expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47];
+
+        expect(getPrimes(50).toString()).to.equal(expected.toString());
+    });
+
+    it("15", function() {
+
+        expected = [2, 3, 5, 7, 11, 13];
+
+        expect(getPrimes(15).toString()).to.equal(expected.toString());
+    });
+
+    it("14", function() {
+
+        expected = [2, 3, 5, 7, 11, 13];
+
+        expect(getPrimes(14).toString()).to.equal(expected.toString());
+    });
+
+
+    it("89", function() {
+
+        expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89];
+
+        expect(getPrimes(89).toString()).to.equal(expected.toString());
+    });
+
+
+    it("76", function() {
+
+        expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73];
+
+        expect(getPrimes(76).toString()).to.equal(expected.toString());
+    });
+
+
+    it("100", function() {
+
+        expected = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+        expect(getPrimes(100).toString()).to.equal(expected.toString());
     });
  });
